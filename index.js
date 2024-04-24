29d84e4de8e09964be6cd916bdde3884350a33f8
Ansari Ahsan <ansariahsan67@gmail.com>
2024-04-24 13:21:47 +0530

    understand modules in node.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const fs = require("fs");
+// custom module example
+/* 1. local modules
+2. built in module which provides node 
+3. third party modules 
+*/
+// example of local modules
+// const math = require('./math') or
+const {add , subtract} = require('./math')
 
 // by default thereads are 4 but we can set it machine by machine
 
@@ -19,3 +27,7 @@ fs.readFile("test.js", "utf-8", (err, result) => {
 });
 setTimeout(()=>{console.log('inside settimeout')},0)
 console.log("after file read");
+
+// console.log(math.add(5,8))
+console.log(add(5,9));
+console.log(subtract(6,8))
