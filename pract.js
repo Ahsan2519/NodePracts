900cef5bbec4bacee213249d8033897f15f936b2
Ansari Ahsan <ansariahsan67@gmail.com>
2024-04-25 18:36:20 +0530

    understand how to send data and html response

--- pract.js
+++ pract.js
@@ -31,11 +31,66 @@ const fs = require("fs");
 
 // with queryParam and parsing url using url package
 
-const url = require("url");
+// const url = require("url");
+
+// const server = http.createServer((req, res) => {
+//   const myUrl = url.parse(req.url, true);
+//   console.log(myUrl);
+//   const currentDate = new Date();
+//   const log = `${currentDate.toLocaleString()}: New Request Received\n `;
+//   if (req.url === "/favicon.ico") return res.end();
+//   fs.appendFile("log.txt", log, (err, result) => {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//       switch (myUrl.pathname) {
+//         case "/":
+//           res.end("Home Page");
+//           break;
+//         case "/about":
+//           res.end(`Hey, ${myUrl.query.username}`);
+//           break;
+//         default:
+//           res.end("404 page not found");
+//       }
+//     }
+//   });
+// });
+
+// server.listen(8000, () => console.log("server is listening at 3000 port"));
+
+// // sending data
+// const server = http.createServer((req, res) => {
+//   const currentDate = new Date();
+//   const log = `${currentDate.toLocaleString()}: New Request Received\n `;
+//   const user = {
+//     userName: "john doe",
+//     userId: 1,
+//   };
+//   if (req.url === "/favicon.ico") return res.end();
+//   fs.appendFile("log.txt", log, (err, result) => {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//       switch (req.url) {
+//         case "/":
+//           res.writeHead(200, { "Content-Type": "application/json" });
+//           res.end(JSON.stringify(user));
+//           break;
+//         case "/about":
+//           res.end("About Page");
+//           break;
+//         default:
+//           res.end("404 page not found");
+//       }
+//     }
+//   });
+// });
+// server.listen(8000, () => console.log("server is listening at 3000 port"));
+
+// sending html
 
 const server = http.createServer((req, res) => {
-  const myUrl = url.parse(req.url, true);
-  console.log(myUrl);
   const currentDate = new Date();
   const log = `${currentDate.toLocaleString()}: New Request Received\n `;
   if (req.url === "/favicon.ico") return res.end();
@@ -43,12 +98,15 @@ const server = http.createServer((req, res) => {
     if (err) {
       console.error(err);
     } else {
-      switch (myUrl.pathname) {
+      switch (req.url) {
         case "/":
-          res.end("Home Page");
+          res.writeHead(200, { "Content-Type": "text/html" });
+          fs.readFile("index.html", "utf-8", (err, result) => {
+            res.end(result);
+          });
           break;
         case "/about":
-          res.end(`Hey, ${myUrl.query.username}`);
+          res.end("About Page");
           break;
         default:
           res.end("404 page not found");
@@ -56,5 +114,12 @@ const server = http.createServer((req, res) => {
     }
   });
 });
-
 server.listen(8000, () => console.log("server is listening at 3000 port"));
+
+/* https  
+1. GET
+2. POST
+3. PUT
+4. PATCH
+5. DELETE
+*/
